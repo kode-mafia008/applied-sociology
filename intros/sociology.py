ba4092403f3884245a281a598f677ae1b1522978
Devs@HathTech <dev@hathtech.com>
2022-01-16 09:29:14 +0545

    intro added

---
+++ intros/sociology.py
@@ -0,0 +1,28 @@
+
+#! Sociology
+#Applied sociology is the application of sociological ideas, concepts, theories and models to address 
+# everyday problems or situations. These may arise in the workplace, in homes,in communities or in 
+# organisations.The means and methods for artificial improvement of social conditions, on the part of man
+# and society as conscious and intelligent agents.” -the term ‘applied sociology’ has been the most general
+# and oldest way of identifying this definition given by Lester F. Ward in 1903. It utilizes “sociological 
+# knowledge and research skills” to gain empirical knowledge. This knowledge is then used to inform clients,
+# decision-makers, and the public regarding various social processes, problems, issues, and conditions to 
+# facilitate their informed decision-making and improvement of life quality. Applied sociology includes needs 
+# assessment, social indicators, evaluation and market research, demographics, and also “directed sociological 
+# research” in education, work, medicine, military, complex organizations, and mental health- in its broadest 
+# sense. (Perlstadt, 2007)
+
+#!Origin of Applied Sociology 
+#Lester F. Ward was the first to bring the concept of applied sociology into the discipline of sociology.
+# In 1883, Ward published a book titled, ‘Dynamic Sociology or Applied Social Science,’ wherein he defined
+# the ability of legislators and the government to bring an improvement in societal conditions by gaining
+# knowledge of the social forces, applying this knowledge, and becoming social scientists. Ward was 
+# strongly opposed to the idea of radical socialist movements and utopian reform that led to abrupt 
+# changes. According to Ward, applied sociology paved the pathway for the application of sociological 
+# principles by serving as a guide for political and social action. (Perlstadt, 2007) Therefore, through 
+# his work, Ward aimed at establishing the groundwork for the distinction between causal processes and the
+# plausible interventions that can be made in these processes to bring about social progress.
+
+#As stated by Hegel, in order to understand and grasp a concept, it is important to know what it is not. 
+# (Marx’s Socialism) Therefore, the next section of the paper carries a differentiation between applied 
+# and pure sociology to facilitate the reader’s understanding of this subject.
